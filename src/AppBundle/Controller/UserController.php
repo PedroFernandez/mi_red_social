1182ac239921f97ac037164c39dd9e077febe1f8
PedroFernandez <fdez.pedro@gmail.com>
2017-07-22 18:24:46 +0200

    create User and Publication Controllers and routing

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserController extends Controller
+{
+    public function loginAction(Request $request)
+    {
+        echo "Accion de login";
+        die;
+    }
+}
--- src/BackendBundle/Controller/DefaultController.php
+++ src/BackendBundle/Controller/DefaultController.php
@@ -2,12 +2,22 @@
 
 namespace BackendBundle\Controller;
 
+use BackendBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $em = $this->getDoctrine()->getManager();
+
+        $user_repo = $em->getRepository('BackendBundle:User');
+
+        /** @var User $user */
+        $user = $user_repo->findAll()[0];
+
+        echo "Welcome Mr: " . $user->getName() . " " . $user->getEmail() . "\n";
+die;
         return $this->render('BackendBundle:Default:index.html.twig');
     }
 }
