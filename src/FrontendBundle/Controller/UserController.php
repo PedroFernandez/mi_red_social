be6eed071821437e3bfd7930b9dd7e0acf4fc1dc
PedroFernandez <fdez.pedro@gmail.com>
2017-07-25 20:48:52 +0200

    register a new user

--- src/FrontendBundle/Controller/UserController.php
+++ src/FrontendBundle/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace FrontendBundle\Controller;
 
 use BackendBundle\Entity\User;
+use Doctrine\ORM\EntityManager;
 use FrontendBundle\Form\RegisterType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,6 +20,49 @@ class UserController extends Controller
     public function registerAction(Request $request) {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                /** @var EntityManager $em */
+                $em = $this->getDoctrine()->getManager();
+
+                $query = $em
+                    ->createQuery('SELECT u FROM BackendBundle:User u WHERE u.email = :email OR u.nick = :nick')
+                    ->setParameter('email', $form->get('email')->getData())
+                    ->setParameter('nick', $form->get('nick')->getData());
+
+                $user_isset = $query->getResult();
+
+                if (count($user_isset) == 0) {
+                    $factory = $this->get('security.encoder_factory');
+                    $encoder = $factory->getEncoder($user);
+
+                    $password = $encoder->encodePassword($form->get('password')->getData(), $user->getSalt());
+
+                    $user->setPassword($password);
+                    $user->setRole('ROLE_USER');
+                    $user->setImage(null);
+
+                    $em->persist($user);
+                    $flush = $em->flush();
+
+                    if ($flush === null) {
+                        $status = 'Te has registrado correctamente!';
+
+                        return $this->redirect('login');
+                    } else {
+                        $status = 'No te has registrado correctamente :(';
+                    }
+
+                } else {
+                    $status = 'El usuario ya existe.';
+                }
+
+            } else {
+                $status = "No te has registrado correctamente.";
+            }
+        }
 
         return $this->render('FrontendBundle:User:register.html.twig', [
             'form' => $form->createView()
