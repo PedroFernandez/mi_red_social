bb1072dcd4c556107382b6b37c125a5536890ff6
PedroFernandez <fdez.pedro@gmail.com>
2017-09-10 12:31:46 +0200

    create action to follow user on Controller

--- src/FrontendBundle/Controller/FollowingController.php
+++ src/FrontendBundle/Controller/FollowingController.php
@@ -2,7 +2,9 @@
 
 namespace FrontendBundle\Controller;
 
+use BackendBundle\Entity\Following;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 
@@ -15,7 +17,26 @@ class FollowingController extends Controller
 
     public function followingAction(Request $request)
     {
-        echo 'following';
-        die();
+        $user = $this->getUser();
+        $followed_id = $request->get('followed');
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $user_repo = $em->getRepository('BackendBundle:User');
+        $followed = $user_repo->find($followed_id);
+
+        $following = new Following();
+        $following->setUser($user);
+        $following->setFollowed($followed);
+
+        $em->persist($following);
+        $flush = $em->flush();
+
+        if ($flush == null) {
+            $status = 'Se ha registrado correctamente!';
+        } else {
+            $status = 'No se ha registrado correctamente';
+        };
+        
+        return new Response($status);
     }
 }
