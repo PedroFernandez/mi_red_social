234db029195ff54d8e79e9ef0ca3f5131798e76d
PedroFernandez <fdez.pedro@gmail.com>
2017-09-25 18:24:47 +0200

    save like to the DB

--- src/FrontendBundle/Controller/LikeController.php
+++ src/FrontendBundle/Controller/LikeController.php
@@ -1,15 +1,34 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: pedro
- * Date: 25/9/17
- * Time: 17:54
- */
 
 namespace FrontendBundle\Controller;
 
+use BackendBundle\Entity\Like;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
-class LikeController
+class LikeController extends Controller
 {
+    public function likeAction($id)
+    {
+        $user = $this->getUser();
+        $em = $this->getDoctrine()->getManager();
+        $publication_repo = $em->getRepository('BackendBundle:Publication');
 
+        $publication = $publication_repo->find($id);
+
+        $like = (new Like())
+            ->setUser($user)
+            ->setPublication($publication);
+
+        $em->persist($like);
+        $flush = $em->flush();
+
+        if ($flush == null) {
+            $status = 'Te gusta esta publicación :) ';
+        } else {
+            $status = 'Error al guardar el Me gusta :(';
+        }
+
+        return new Response($status);
+    }
 }
