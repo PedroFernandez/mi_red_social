852418b14c5578b0f79981705f08dd7d01a16b85
PedroFernandez <fdez.pedro@gmail.com>
2017-10-08 12:19:19 +0200

    NotificationService

--- src/FrontendBundle/Services/NotificationService.php
+++ src/FrontendBundle/Services/NotificationService.php
@@ -1,12 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: pedro
- * Date: 8/10/17
- * Time: 11:09
- */
+
+namespace FrontendBundle\Services;
+
+use BackendBundle\Entity\Notification;
 
 class NotificationService
 {
+    private $manager;
 
+    public function __construct($manager)
+    {
+        $this->manager = $manager;
+    }
+
+    public function set($user, $type, $id, $extra = null)
+    {
+        $notification = new Notification();
+        $notification->setUser($user);
+        $notification->setType($type);
+        $notification->setTypeId($id);
+        $notification->setReaded(0);
+        $notification->setCreatedAt(new \DateTime('now'));
+        $notification->setExtra($extra);
+
+        /** @var \Doctrine\ORM\EntityManager $em */
+        $em = $this->manager;
+        $em->persist($notification);
+        $flush = $em->flush();
+
+        if ($flush == null) {
+            $status = true;
+        } else {
+            $status = false;
+        }
+
+        return $status;
+    }
 }
